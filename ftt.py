d20b4cea636f03a4d73693081ceb9b83337fb8ff
Chrisovalantis Vatos <chrisovalantis.vatos@mail.mcgill.ca>
2025-11-18 14:21:13 -0500

    Mode 1 Function

--- ftt.py
+++ ftt.py
@@ -1,8 +1,17 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import argparse
 import cv2
+import os
+from matplotlib.colors import LogNorm
 
-
+# -----------------------------------------------------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------------------------------------------------#
+# -------------------------------- Fourier and Inverse Fourier Transformation Functions -------------------------------- #
+# -----------------------------------------------------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------------------------------------------------#
 def dft(arr: np.ndarray) -> np.ndarray:
     N = arr.size
     n = np.arange(N)
@@ -30,9 +39,8 @@ def fft(arr: np.ndarray) -> np.ndarray:
     return result
 
 def inverse_fft(arr: np.ndarray) -> np.ndarray:
-
     # Define an inner recursive function for inverse FFT to 
-    # that performed the divide and conquer approach
+    # handle the divide and conquer approach
     def inverse_fft_recursive(arr: np.ndarray) -> np.ndarray:
         N = arr.size
 
@@ -76,17 +84,102 @@ def inverse_fft_2d(freqs: np.ndarray) -> np.ndarray:
     img = np.transpose([inverse_fft(row) for row in transformed_rows])
     return img
 
-def denoising_img():
+def is_power_of_2(n: int) -> bool:
+    # A number n is a power of 2 iff it is positive and only a single bit 
+    # is set in its binary representation. We can easily check this with a bitwise AND operations
+    return (n > 0) and (n & (n - 1) == 0)
+
+def load_img(img_path: str) -> np.ndarray:
+    # Read the image using cv2
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+
+    # As per the assignment requirements, we need to ensure that the image dimensions are powers of 2
+    height, width = img.shape
+    
+    # If the current height is not a power of 2, acquire the next power of 2
+    if not is_power_of_2(height):
+        height = 2 ** (height - 1).bit_length() 
+
+    # If the current width is not a power of 2, acquire the next power of 2
+    if not is_power_of_2(width):
+        width = 2 ** (width - 1).bit_length() 
+
+    # Resize the image to the new dimensions which are powers of 2    
+    resized_img = cv2.resize(img, (width, height))
+    return np.array(resized_img)
+
+# -----------------------------------------------------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------------------------------------------------#
+# --------------------------------------------- Functions for Modes 1 to 4 --------------------------------------------- #
+# -----------------------------------------------------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------------------------------------------------#
+
+def mode_1_fft_img(img: np.ndarray) -> np.ndarray:
+    # Obtain the 2D FFT of the image
+    fast_fourier_transformed_img = fft_2d(img)
+
+    # Output a one by two subplot of the original image and next to it its Fourier transform
+    # Plot original image
+    plt.subplot(1, 2, 1)
+    plt.title("Original Image", fontweight="bold")
+    plt.imshow(img, cmap='gray')
+
+    # Plot 2D Fast Fourier Transformed image
+    plt.subplot(1, 2, 2)
+    plt.title("Fast Fourier Transform (FFT) of Image (Log Scaled)", fontweight="bold")
+    plt.imshow(np.abs(fast_fourier_transformed_img), cmap='gray', norm=LogNorm())
+
+    plt.show()
+
+def mode_2_denoise_img():
     return None
 
 def compress_img():
     return None
 
-def load_img():
-    return None
+def parse_command_line_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", help="Mode to run program in", type=int, action="store", default=1)
+    parser.add_argument("-i", help="Filename of the image we wish to take the DFT of", type=str, action="store", default="./assets/moonlanding.png")
+    args = parser.parse_args()
+    return args
 
 def main():
-    return None
+    # Parse the command line arguments m (mode) and i (image)
+    args = parse_command_line_args()
+
+    # Ensure input image exists before calling corresponding functions based on mode
+    # Check if the provided image path exsists; display error message if it does not
+    # or if there was an error loading the image
+    source_img = None
+    try:
+        if not os.path.exists(args.i):
+            print(f"Error: The specified image path '{args.i}' does not exist.")
+            return
+        # Load the image from the specified path if the provided is valid and exists
+        else:
+            source_img = load_img(args.i)
+    except Exception as e:
+        print(f"An error occurred while loading the image: {e}")
+        return
+
+    # Calling the correct functions based on input parameters after verifying image path
+    if args.m == 1:
+        mode_1_fft_img(source_img)
+    elif args.m == 2:
+        return None
+    elif args.m == 3:
+        return None
+    elif args.m == 4:
+        return None
+    else :
+        print("Invalid mode selected. Please choose a mode between 1 and 4.")
+        return None
+    
+
+
 
 
 if __name__ == "__main__":
